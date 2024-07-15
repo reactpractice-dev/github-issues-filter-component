beb2d746577463bbdafaf1cc7382da495f82937c
Corina Udrescu <corinutz@gmail.com>
2024-07-15 09:06:33 +0200

    popup the filter container

--- src/components/GithubFilter.jsx
+++ src/components/GithubFilter.jsx
@@ -1,20 +1,31 @@
 import { FaAngleDown } from "react-icons/fa6";
 import { useState } from "react";
+import { useFloating } from "@floating-ui/react";
 
 const GithubFilter = ({ name }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const { refs, floatingStyles } = useFloating();
   return (
     <div>
       <button
         className="flex items-center gap-2"
         onClick={() => setIsOpen(!isOpen)}
+        ref={refs.setReference}
       >
         {name}
         <span>
           <FaAngleDown />
         </span>
       </button>
-      {isOpen ? <div className="bg-white p-4">Open</div> : null}
+      {isOpen ? (
+        <div
+          ref={refs.setFloating}
+          style={floatingStyles}
+          className="bg-white p-4 border border-gray-200 shadow-md"
+        >
+          Open
+        </div>
+      ) : null}
     </div>
   );
 };
