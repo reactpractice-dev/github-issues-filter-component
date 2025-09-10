03a534444ca7e59b2c8808ffcfda081cd87302af
Corina Udrescu <corinutz@gmail.com>
2025-09-10 09:19:12 +0200

    show floating element

--- src/components/GithubFilter.tsx
+++ src/components/GithubFilter.tsx
@@ -1,5 +1,6 @@
 import { ChevronDown } from "lucide-react";
 import { useState } from "react";
+import { useFloating } from "@floating-ui/react";
 
 type Props = {
   title: string;
@@ -7,16 +8,25 @@ type Props = {
 
 const GithubFilter: React.FC<Props> = ({ title }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const { refs, floatingStyles } = useFloating();
   return (
-    <button
-      className={`flex items-center gap-2 p-3 cursor-pointer ${
-        isOpen ? "bg-gray-100" : ""
-      }`}
-      onClick={() => setIsOpen(!isOpen)}
-    >
-      {title}
-      <ChevronDown className="w-4 h-4" />
-    </button>
+    <>
+      <button
+        className={`flex items-center gap-2 p-3 cursor-pointer ${
+          isOpen ? "bg-gray-100" : ""
+        }`}
+        onClick={() => setIsOpen(!isOpen)}
+        ref={refs.setReference}
+      >
+        {title}
+        <ChevronDown className="w-4 h-4" />
+      </button>
+      {isOpen && (
+        <div ref={refs.setFloating} style={floatingStyles}>
+          Tooltip
+        </div>
+      )}
+    </>
   );
 };
 
